c17a78271b4ba1b802a11ec9a484a788dde723e0
yuki <kimaira7@gmail.com>
2013-03-25 14:52:52 +0900

    連想ノートリストの関連度の表示方法を少しだけ変更。

--- src/cx/fbn/nevernote/gui/RensoNoteListItem.java
+++ src/cx/fbn/nevernote/gui/RensoNoteListItem.java
@@ -83,21 +83,25 @@ public class RensoNoteListItem extends QWidget{
 		
 		// 項目の中身
 		// フォント設定
-		painter.setPen(QColor.black);
+		QFont relationFont = new QFont();
+		relationFont.setPixelSize(35);
+		relationFont.setBold(true);
 		QFont titleFont = new QFont();
 		titleFont.setPixelSize(15);
 		titleFont.setBold(true);
 		QFont normalFont = new QFont();
 		normalFont.setPixelSize(12);
 		
-		// タイトル
-		painter.setFont(titleFont);
-		painter.drawText(85, 3, size().width() - 135, 20, Qt.AlignmentFlag.AlignLeft.value(), noteTitle);
 		// 関連度
 		double ratio = (double)relationPoints / allPointSum;
 		int green = (int) (255 * (1.0 - ratio));
-		painter.setPen(new QColor(255, green, 0));
-		painter.drawText(size().width() - 45, 3, 45, 20, Qt.AlignmentFlag.AlignRight.value(), String.valueOf((int)(ratio * 100)) + "%");
+		painter.setPen(new QColor(255, green, 0, 120));
+		painter.setFont(relationFont);
+		painter.drawText(85, size().height() - 40, size().width() - 85, 40, Qt.AlignmentFlag.AlignRight.value(), String.valueOf((int)(ratio * 100)) + "%");
+		// タイトル
+		painter.setPen(QColor.black);
+		painter.setFont(titleFont);
+		painter.drawText(85, 3, size().width() - 85, 20, Qt.AlignmentFlag.AlignLeft.value(), noteTitle);
 		// ノート作成日時
 		painter.setFont(normalFont);
 		painter.setPen(new QColor(60, 65, 255));
